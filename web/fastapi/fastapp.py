98a02767eb9987e91ca4afcbb75eacafac4cc569
janetat <allinhippo@gmail.com>
2021-12-19 15:45:33 +0800

    update fastapp.py

--- web/fastapi/fastapp.py
+++ web/fastapi/fastapp.py
@@ -2,12 +2,13 @@ import html
 import requests
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 k8s_svc_url = 'http://10.98.141.216:9999'
 
-@app.get("/")
-def index():
+@app.get("/nginx", response_class=HTMLResponse)
+def nginx():
     r = requests.get(k8s_svc_url, timeout=5)
     
     response = html.unescape(r.text)
